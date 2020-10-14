f80233d6e3f084cf253969f9d8813f361811ecff
lucindaloane <72323614+lucindaloane@users.noreply.github.com>
2020-10-14 11:43:31 +1100

    My solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#double arrow redefines an argument only in the current function environment (allows 2 levels of parameters)
+makeCacheMatrix <- function(x=matrix()){
+  inv <- NULL
+  #sets and gets value of matrix
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  #sets and gets value of inverse matrix
+  setInverse <- function(inverse){
+    inv <<- inverse
+  }
+  getInverse <- function() {
+    inv
+  }
+  list (set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#computing matrix - 'solves' for the inverse
+cacheSolve <- function(x,...) {
+  inv <- x$getInverse()
+  if(!is.null(inv)) {
+    message("getting cached inverse")
+  }
+  matr <- x$get()
+  inv <- solve(matr,...)
+  x$setInverse(inv)
+  inv
 }
+
+#makes practice matrix
+prac <- makeCacheMatrix(matrix(1:4, nrow = 2, ncol = 2))
+prac$get()
+#Tries to get inverse of matrix (returns null if nothing has been cached)
+prac$getInverse()
+cacheSolve(prac)
